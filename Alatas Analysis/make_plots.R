0d66d5c62e38829ec5d7a1d1589c5cdc5fe02eab
LendieFollett <lendie.follett@drake.edu>
2021-12-13 12:18:15 -0600

    name

--- Alatas Analysis/make_plots.R
+++ Alatas Analysis/make_plots.R
@@ -115,12 +115,12 @@ all_coef %>%merge(variable_labels, by.x = "parameter", by.y = "Name") %>%
   #facet_grid(Category~., scales = "free_y")+
   coord_flip() + 
   theme_bw() +
-  labs(x = "", y = "Standardized Coefficient Estimate \n (Relative to harmonic mean)") +
+  labs(x = "", y = "Standardized Coefficient Estimate") +
   scale_fill_grey("Method")+
   theme(legend.position = c(.9,.9), 
         legend.box.background = element_rect(colour = "black"))
 
-ggsave("Alatas Analysis/coef_score.pdf", width = 12, height = 12)
+ggsave("Alatas Analysis/coef_score_alatas.pdf", width = 12, height = 12)
 
 all_coef %>%merge(variable_labels, by.x = "parameter", by.y = "Name") %>%
   dplyr::select(Definition, Category, CB_beta_rank_mean, CB_beta_rank_mean_noelite) %>%
@@ -137,12 +137,12 @@ all_coef %>%merge(variable_labels, by.x = "parameter", by.y = "Name") %>%
   #facet_grid(Category~., scales = "free_y")+
   coord_flip() + 
   theme_bw() +
-  labs(x = "", y = "Standardized Coefficient Estimate \n (Relative to harmonic mean)") +
+  labs(x = "", y = "Standardized Coefficient Estimate") +
   scale_fill_grey("Method")+
   theme(legend.position = c(.9,.9), 
         legend.box.background = element_rect(colour = "black"))
 
-ggsave("Alatas Analysis/coef_score_EC.pdf", width = 12, height = 12)
+ggsave("Alatas Analysis/coef_score_EC_alatas.pdf", width = 12, height = 12)
 
 
 
