8d52f9bf024e317c4822130efcd8d69ef0946f06
LendieFollett <lendie.follett@drake.edu>
2022-08-09 11:40:38 -0500

    Update hh_rank_run_mcmc.R

--- Indonesia Analysis/hh_rank_run_mcmc.R
+++ Indonesia Analysis/hh_rank_run_mcmc.R
@@ -18,14 +18,21 @@ iter_burn = 20   ## Gibbs sampler burn-in iterations
 print_opt = 100  ## print a message every print.opt steps
 
 
-all_ranks <- read.csv("Data/Indonesia/Cleaning/hh_rankings.csv") 
+all_ranks <- read.csv("Data/Indonesia/Cleaning/hh_rankings.csv") %>%
+  arrange(hhid_ranked)
 
+ranked <- unique(all_ranks$hhid_ranked)
+rankers <- unique(all_ranks$hhid_ranker)
 #Create Rank matrix in format required for CBTarget()
 Tau2 <- array(NA, dim = c(length(unique(all_ranks$hhid_ranked)), length(unique(all_ranks$hhid_ranker))))
 j = 0
-for ( idx in unique(all_ranks$hhid_ranked)){ #loop over rows
+for ( idx in rankers){ #loop over columns
+  all_ranks_sub <- subset(all_ranks, hhid_ranker == idx)
   j = j + 1
-  Tau2[all_ranks$hhid_ranker == idx,j] <- all_ranks$rank[all_ranks$hhid_ranker == idx]
+  for (idx2 in all_ranks_sub$hhid_ranked){
+    Tau2[ranked == idx2, j] <- all_ranks_sub$rank[all_ranks_sub$hhid_ranked == idx2][1] 
+    #the [1] is because some households ranked another household twice... i just took the first for now
+  }
 }
 
 
